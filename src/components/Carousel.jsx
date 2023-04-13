690e1240aca184afaa19bdc3849fd2b4bdb24a85
Naomi Downing <downing3@bu.edu>
2023-04-13 15:52:10 -0400

    website !!!!!!!

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,21 +2,48 @@ import React from "react";
 import beachImg from "./beach.png";
 import commonsImg from "./commons.png";
 import snowImg from "./snow.png";
+import fijiBeach from "./fijiBeach.png";
+import fiji from "./fiji.png";
+import Fbeach from "./Fbeach.png";
+import highlands from "./highlands.png";
+import clothes from "./clothes.png";
+import sky from "./sky.png";
+
+
+
+
+
+
 
 function Carousel() {
   return (
     <div className="w-64 carousel rounded-none">
   <div className="carousel-item w-full">
-    <img src={beachImg} className="w-full" alt="Tailwind CSS Carousel component" />
+    <img src={beachImg} className="w-full" alt="img of beach, florida" />
   </div> 
   <div className="carousel-item w-full">
-    <img src={commonsImg} className="w-full" alt="Tailwind CSS Carousel component" />
+    <img src={commonsImg} className="w-full" alt="img of boston commons" />
   </div> 
   <div className="carousel-item w-full">
-    <img src={snowImg} className="w-full" alt="Tailwind CSS Carousel component" />
+    <img src={snowImg} className="w-full" alt="img of boston, winter" />
   </div> 
   <div className="carousel-item w-full">
-    <img src={beachImg} className="w-full" alt="Tailwind CSS Carousel component" />
+    <img src={fijiBeach} className="w-max" alt="img of beach" />
+  </div>
+  <div className="carousel-item w-full">
+    <img src={fiji} className="w-full" alt="img of fiji" />
+  </div>
+  <div className="carousel-item w-full">
+    <img src={Fbeach} className="w-full" alt="img of beach, fiji" />
+  </div>
+  <div className="carousel-item w-full">
+    <img src={highlands} className="w-full" alt="img of highlands" />
+  </div>
+  <div className="carousel-item w-full">
+    <img src={clothes} className="w-full" alt="img of clothesline" />
+  </div>
+  <div className="carousel-item w-full">
+    <img src={sky} className="w-full" alt="img of sky" />
   </div>
 </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import resume from "./Resume.pdf"
 
 function Footer() {
-    return(
-        <footer className="footer footer-center p-4 bg-transparent font-light text-white">
-  <div>
-    <p>linkedin             |                   resume</p>
-  </div>
-</footer>
-    );
+  return (
+    <footer className="footer flex justify-center p-4 bg-transparent font-light text-white">
+      <div className="flex justify-center space-x-4">
+        <a
+          className="hover:text-gray-400 transition-colors duration-300"
+          href="https://www.linkedin.com/in/naomi-downing"
+        >
+          linkedin
+        </a>
+        <a
+            className="hover:text-gray-400 transition-colors duration-300"
+            href={resume}
+            download="naomidowningresume.pdf"
+        >
+            resume
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
