ef2f1cae8ecce098b22b90b2eb19eb159cfe81dc
Naomi Downing <downing3@bu.edu>
2023-04-13 16:42:02 -0400

    changes
    changes

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import NavBar from "./NavBar";
+import img from "../Assets/Images/IMG_4178.png";
 
 function AboutMe() {
-    return (
-        <>
-         <div className="bg-sage h-screen">
-            <NavBar />
-                <p>hi! about me:</p>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div
+        className="bg-sage h-screen flex flex-col justify-center items-center"
+        style={{ backgroundImage: `url(${img})` }}
+      >
+        <div className="fixed top-0 left-0 right-0">
+          <NavBar />
+        </div>
+        <div className="w-full">
+          <div className="text-left ml-8 -mt-10">
+            <p className="text-5xl text-white font-thin mt-12">hi, i'm naomi!</p>
+          </div>
+        </div>
+        <div className="w-full">
+          <div className="text-left ml-8 mr-40">
+            <p className="text-5xl text-white font-thin mt-12 bg-black">
+              who am i
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AboutMe;
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -39,7 +39,7 @@ function ContactForm() {
               id="message"
               rows="6"
               className="block p-2.5 w-96 text-sm text-gray-900 bg-gray-50 rounded-none shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="Leave a comment..."
+              placeholder="leave a comment..."
             ></textarea>
           </div>
           <button
