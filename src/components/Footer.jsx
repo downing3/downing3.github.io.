7f3b161d38fd824278020f81d214af377908ab62
Naomi Downing <downing3@bu.edu>
2023-04-20 17:06:01 -0400

    color change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,13 @@ function Footer() {
     <footer className="footer flex justify-center p-4 bg-transparent font-light text-white">
       <div className="flex justify-center space-x-4">
         <a
-          className="hover:text-gray-400 transition-colors duration-300"
+          className="hover:text-buttons transition-colors duration-300"
           href="https://www.linkedin.com/in/naomi-downing"
         >
           linkedin
         </a>
         <a
-            className="hover:text-gray-400 transition-colors duration-300"
+            className="hover:text-buttons transition-colors duration-300"
             href={resume}
             download="naomidowningresume.pdf"
         >
