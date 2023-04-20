b849d85ff19b98632b988baceb600d6a8d4fb565
Naomi Downing <downing3@bu.edu>
2023-04-20 10:52:16 -0400

    edits
    edits

--- src/components/DarkNavbar.jsx
+++ src/components/DarkNavbar.jsx
@@ -7,7 +7,7 @@ function DarkNavbar() {
     <nav className="bg-transparent py-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-center">
         <div className="flex items-center">
-          <Link to="/hero">
+          <Link to="/">
             <span className="text-white bg-black text-2xl font-thin italic">naomi mendocino downing</span>
           </Link>
         </div>
